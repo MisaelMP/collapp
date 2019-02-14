0a474eb5977b47832652c705376e20c48dc2ba31
Misael M <phatarino@gmail.com>
2019-02-14 20:06:24 +1100

    create a ui ti new posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+before_action :redirect_if_not_signed_in, only: [:new]
 # top-level class
 class PostsController < ApplicationController
   def show
@@ -16,6 +17,21 @@ class PostsController < ApplicationController
 
   def team
     posts_for_branch(params[:action])
+  end
+
+  def new
+    @branch = params[:branch]
+    @categories = Category.where(branch: @branch)
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      redirect_to root_path
+    end
   end
 
   private
@@ -35,5 +51,10 @@ class PostsController < ApplicationController
       category: params[:category],
       branch: params[:action]
     ).call
+  end
+
+  def post_params
+    params.require(:post).permit(:content, :title, :category_id)
+          .merge(user_id: current_user.id)
   end
 end
